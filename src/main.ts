e9d9d6e2bcc17b66453a7a151dd0357b116787a0
minericefield <minericefield@gmail.com>
2021-05-29 01:38:11 +0900

    update basic file structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { getApp } from './app';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await getApp();
+
   await app.listen(3000);
 }
 bootstrap();
