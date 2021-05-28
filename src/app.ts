5abaca36aa788dcba4ff9bc6a16f24a1c1cf825a
minericefield <minericefield@gmail.com>
2021-05-29 02:11:00 +0900

    define app type as express

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './core/app.module';
 
 export async function getApp() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   return app;
 }
