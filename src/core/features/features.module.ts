37bbbecb348eb6920491e348157c06cce2de5c52
minericefield <minericefield@gmail.com>
2021-05-31 00:16:13 +0900

    update features module

--- src/core/features/features.module.ts
+++ src/core/features/features.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { AdminsModule } from './admins/admins.module';
+import { AuthAdminsModule } from './auth-admins/auth-admins.module';
+import { RolesModule } from './roles/roles.module';
 import { TopModule } from './top/top.module';
 
 @Module({
-  imports: [TopModule],
+  imports: [TopModule, AdminsModule, AuthAdminsModule, RolesModule],
 })
 export class FeaturesModule {}
