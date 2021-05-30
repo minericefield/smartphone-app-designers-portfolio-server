23dc52423db292dca31e719c4634605638f84738
minericefield <minericefield@gmail.com>
2021-05-31 01:17:55 +0900

    add test cases

---
+++ __tests__/services/admins.service.spec.ts
@@ -0,0 +1,50 @@
+import { INestApplication } from '@nestjs/common';
+
+import { ROLES, ADMIN_STATUS } from '../../src/core/commons/constants';
+import { SimpleHashingService } from '../../src/core/commons/helpers/simple-hashing.service';
+import { AdminsService } from '../../src/core/features/admins/admins.service';
+import { getInitializedAppForService, doneApp } from '../../test/helper';
+
+describe('Admins Service', () => {
+  let app: INestApplication;
+  let adminsService: AdminsService;
+
+  beforeEach(async () => {
+    app = await getInitializedAppForService();
+
+    adminsService = app.get<AdminsService>(AdminsService);
+  });
+
+  it('password would not be saved directly', async () => {
+    await adminsService.create({
+      email: 'admin@example.com',
+      password: 'Password1234_',
+      role: ROLES['FULL CONTROL'],
+      status: ADMIN_STATUS.ACTIVATED,
+      canVerify: false,
+    });
+    const admin = await adminsService.findOneByEmail('admin@example.com');
+    expect(admin.password).not.toBe('Password1234_');
+  });
+
+  it('password would be saved by comparable hash', async () => {
+    await adminsService.create({
+      email: 'admin@example.com',
+      password: 'Password1234_',
+      role: ROLES['FULL CONTROL'],
+      status: ADMIN_STATUS.ACTIVATED,
+      canVerify: false,
+    });
+    const simpleHashingService =
+      app.get<SimpleHashingService>(SimpleHashingService);
+    const admin = await adminsService.findOneByEmail('admin@example.com');
+    expect(
+      await simpleHashingService.compare('Password1234_', admin.password),
+    ).toBeTruthy();
+  });
+
+  afterEach(async (done) => {
+    await doneApp(app);
+    done();
+  });
+});
