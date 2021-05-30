8bf951dab552b441ccc4792c2196b3de5cada41f
minericefield <minericefield@gmail.com>
2021-05-31 00:11:15 +0900

    add auth admins module

---
+++ src/core/features/auth-admins/auth-admins.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+
+import { SimpleHashingService } from '../../commons/helpers/simple-hashing.service';
+import { SessionSerializer } from '../../commons/serializers/session.serializer';
+import { AdminsModule } from '../admins/admins.module';
+
+import { AuthRendererController } from './auth-admins-renderer.controller';
+import { AuthAdminsService } from './auth-admins.service';
+import { LocalStrategy } from './strategies/local.strategy';
+
+@Module({
+  imports: [AdminsModule, PassportModule],
+  controllers: [AuthRendererController],
+  providers: [
+    AuthAdminsService,
+    LocalStrategy,
+    SessionSerializer,
+    SimpleHashingService,
+  ],
+})
+export class AuthAdminsModule {}
