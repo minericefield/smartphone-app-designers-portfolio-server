584a632cfaafeab4e05f68617616a539d75e7275
minericefield <minericefield@gmail.com>
2021-05-30 23:38:39 +0900

    add roles service

---
+++ src/core/features/roles/roles.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { CreateRoleDto } from './dto/';
+import { Role, RoleDocument } from './schemas/role.schema';
+
+@Injectable()
+export class RolesService {
+  constructor(
+    @InjectModel(Role.name) private readonly roleModel: Model<RoleDocument>,
+  ) {}
+
+  findAll() {
+    return this.roleModel.find().sort('_id').exec();
+  }
+
+  async create(createRoleDto: CreateRoleDto) {
+    return new this.roleModel(createRoleDto).save();
+  }
+}
