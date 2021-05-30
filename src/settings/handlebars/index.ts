9dcad5ee900704d53b71cde362f09b0e18f5da38
minericefield <minericefield@gmail.com>
2021-05-31 01:07:48 +0900

    add hbs helpers

--- src/settings/handlebars/index.ts
+++ src/settings/handlebars/index.ts
@@ -1,5 +1,25 @@
+import { Document } from 'mongoose';
+import { path, init } from 'ramda';
+
+import { ROLES, ADMIN_STATUS } from '../../core/commons/constants';
+import { AdminDocument } from '../../core/features/admins/schemas/admin.schema';
+
 export const handlebarHelpers = {
-  getSomething: function () {
-    return 'something';
+  docToObject: function (doc: Document) {
+    return doc.toObject();
+  },
+  getValueFromDoc: function (doc: Document, ...keys: string[]) {
+    keys = init(keys);
+    return path(keys, doc);
+  },
+  // role referenced admin
+  isBrowsingControl: function (admin: any) {
+    return admin.role._id === ROLES['BROWSING CONTROL'];
+  },
+  isPending: function (admin: AdminDocument) {
+    return admin.status === ADMIN_STATUS.PENDING;
+  },
+  defaultTo: function (value1: any, value2?: any) {
+    return value2 || value1;
   },
 };
