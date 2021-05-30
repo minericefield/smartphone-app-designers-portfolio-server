759e0ea6fe43671b2ef1f96af9fa1b903d957d11
minericefield <minericefield@gmail.com>
2021-05-31 00:14:47 +0900

    update top controller

--- src/core/features/top/top-renderer.controller.ts
+++ src/core/features/top/top-renderer.controller.ts
@@ -1,10 +1,24 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Render,
+  UseGuards,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
+
+import { AuthenticatedExceptionRendererFilter } from '../../commons/filters/authenticated-exception-renderer.filter';
+import { AuthenticatedGuard } from '../../commons/guards/authenticated.guard';
+import { MeInterceptor } from '../../commons/interceptors/me.interceptor';
 
 @Controller()
+@UseGuards(AuthenticatedGuard)
+@UseFilters(AuthenticatedExceptionRendererFilter)
 export class TopRendererController {
   @Get()
+  @UseInterceptors(MeInterceptor)
   @Render('top')
   root() {
-    return { message: 'Hello World!!!' };
+    return;
   }
 }
