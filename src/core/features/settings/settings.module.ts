f1db9f3a649b24f82eeaab4ec2cebc022c14f144
minericefield <minericefield@gmail.com>
2021-06-23 00:07:55 +0900

    init settings api

--- src/core/features/settings/settings.module.ts
+++ src/core/features/settings/settings.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { Settings, SettingsSchema } from './schemas/settings.schema';
+import { SettingsApiController } from './settings-api.controller';
 import { SettingsRendererController } from './settings-renderer.controller';
 import { SettingsService } from './settings.service';
 
@@ -11,7 +12,7 @@ import { SettingsService } from './settings.service';
       { name: Settings.name, schema: SettingsSchema },
     ]),
   ],
-  controllers: [SettingsRendererController],
+  controllers: [SettingsRendererController, SettingsApiController],
   providers: [SettingsService],
   exports: [SettingsService],
 })
